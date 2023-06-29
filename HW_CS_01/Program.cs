56caba992041239e0039ecea27a6e78506e91582
Dimelena <3apy@mail.ru>
2023-06-29 20:02:07 +0300

    Added HW_CS_01

---
+++ HW_CS_01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Task 2
+// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2) 
+    Console.WriteLine($"Maximum number is {num1} and minimum number is {num2}.");
+else if(num1 == num2)
+    Console.WriteLine("The numbers are the same.");
+else
+    Console.WriteLine($"Maximum number is {num2} and minimum number is {num1}.");
+*/
+
