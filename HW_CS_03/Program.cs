8a8a0fa738b34f1ff6176de0f22bf2f216ae96a6
Dimelena <3apy@mail.ru>
2023-06-30 01:13:08 +0300

    Completion of work in HW_CS_03

--- HW_CS_03/Program.cs
+++ HW_CS_03/Program.cs
@@ -59,3 +59,43 @@ Console.WriteLine($"Distance between points {(Math.Round(Distance(x1, y1, z1, x2
 */
 
 
+
+// Task 3
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+/*
+void CubeNumbers(int N)
+{
+    int num = 1;
+    while (num < N+1)
+    {
+        Console.WriteLine(num+" - "+num*num*num);
+        num++;
+    }
+}
+    Console.Write("Input N: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    CubeNumbers(N);
+*/
+ 
+
+// Вариант со -> в одну строку
+/*
+void CubeNumbers(int N)
+{
+    int num = 1;
+    while (num < N+1)
+    {
+        Console.Write(num*num*num+" - ");
+        num++;
+    }
+}
+    Console.Write("Input N: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write(N+" ->  ");
+    CubeNumbers(N);
+*/
