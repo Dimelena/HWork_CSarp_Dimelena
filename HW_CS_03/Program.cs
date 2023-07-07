adb5735f1057e36656ac5b51460012270d2f746f
Dimelena <3apy@mail.ru>
2023-07-07 11:08:15 +0300

    Added tasks 17 from the seminar 03

--- HW_CS_03/Program.cs
+++ HW_CS_03/Program.cs
@@ -1,4 +1,59 @@
-﻿// Task 19
+﻿
+// Seminar 3
+
+// Task 17.
+// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+// и выдаёт номер четверти плоскости, в которой находится эта точка.
+// Валерия Geek Brains: x=34; y=-30 -> 4
+// x=2; y=4-> 1
+// x=-34; y=-30 -> 3
+
+int GetNumberOfQuarter(int X, int Y)
+{
+int result = 0;
+if (X > 0 && Y > 0)
+{
+result = 1;
+}
+else if (X < 0 && Y > 0)
+{
+result = 2;
+}
+else if (X < 0 && Y < 0)
+{
+result = 3;
+}
+else if (X > 0 && Y < 0)
+{
+result = 4;
+}
+return result;
+}
+
+Console.Write("Enter a variable X: ");
+int userX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a variable Y: ");
+int userY = Convert.ToInt32(Console.ReadLine());
+
+int quarter = GetNumberOfQuarter(userX, userY);
+if (quarter > 0)
+{
+Console.WriteLine($"Point [{userX} : {userY}] is in a quarter {quarter}");
+}
+else
+{
+Console.WriteLine($"Point [{userX} : {userY}] is on the intersection of the planes");
+}
+
+
+
+
+
+
+// Homework 3
+
+
+// Task 19
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, 
 // является ли оно палиндромом. Через строку решать нельзя.
 // 14212 -> нет; 12821 -> да; 23432 -> да
